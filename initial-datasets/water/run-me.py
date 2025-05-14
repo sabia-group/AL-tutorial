17ef01a8ffee47e7421cfdb5a9630758b586b22b
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-05-14 16:32:53 +0200

    fixed and renamed

--- initial-datasets/water/run-me.py
+++ initial-datasets/water/run-me.py
@@ -1,8 +1,9 @@
 from ase.io import read
-print("\n\tReading 'train.extxyz':\n")
-atoms = read("train.extxyz",index=":")
-print("\tn. of structures: ", len(atoms))
-print("\t     n. of atoms: ", atoms[0].get_global_number_of_atoms())
-print("\t            info: ", list(atoms[0].info.keys()))
-print("\t          arrays: ", list(atoms[0].arrays.keys()))
-print()
+for n in range(8):
+    print(f"\n\tReading 'train.{n}.extxyz':")
+    atoms = read(f"train.{n}.extxyz",index=":")
+    print("\tn. of structures: ", len(atoms))
+    print("\t     n. of atoms: ", atoms[0].get_global_number_of_atoms())
+    print("\t            info: ", list(atoms[0].info.keys()))
+    print("\t          arrays: ", list(atoms[0].arrays.keys()))
+    print()
