46c919038e3934f3fdf238a30c3d0a3fbb87ff34
Elia Stocco <elia.stocco@mpsd.mpg.de>
2025-06-03 10:28:27 +0200

    added MD for refined eigen

---
+++ ipi-examples/eigen-refined/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+source "${IPIPATH}/env.sh"
+
+SOCKET=true
+model_folder="../../checkpoints/models-refined"
+
+if ${SOCKET}; then # parallel: this should be faster because the MACE models are run in parallel
+    echo "Starting i-PI socket server..."
+    # i-pi committee4nvt.ffsocket.xml &
+    i-pi RESTART &
+
+    sleep 5  # or better: wait until socket files exist
+
+    for n in {0..3}; do
+        echo "Starting driver $n..."
+        i-pi-py_driver -u -a address-${n} -m mace -o template=start.extxyz,model=${model_folder}/mace.com=${n}_compiled.model &
+    done
+
+    wait
+else # serial
+    echo "Starting i-PI in direct mode..."
+    i-pi committee4nvt.ffdirect.xml
+fi
+
+python ../post-process.py -i ipi.pos_0.extxyz -o eigen.extxyz
